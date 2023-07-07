415e8a25e58e0e3a7e27a1f58dccbb2fd0d75bb6
pratikbin <68642400+pratikbin@users.noreply.github.com>
2023-07-07 22:21:25 +0530

    test: multiple tx concurrently

--- bdjuno-cosmos-docker/scripts/run.sh
+++ bdjuno-cosmos-docker/scripts/run.sh
@@ -2,22 +2,21 @@
 
 set -euxo
 
-SEQUENCE=1
+lockname=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 20 | head -n 1)
+semaphore create --name ${lockname} --size 10
 
-while true;
-do
-    # generating key
-    RANDOM_CHARS=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
-    gaiad --home $NODE1_HOME keys add test$RANDOM_CHARS --keyring-backend test
+while true; do
+  # generating key
+  RANDOM_CHARS=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+  gaiad --home $NODE1_HOME keys add test$RANDOM_CHARS --keyring-backend test
 
-    # get address in ADDR variable
-    ADDR=$(gaiad --home $NODE1_HOME keys show test$RANDOM_CHARS -a --keyring-backend test)
+  # get address in ADDR variable
+  ADDR=$(gaiad --home $NODE1_HOME keys show test$RANDOM_CHARS -a --keyring-backend test)
 
+  (
+    semaphore acquire --name ${lockname}
     # sending 1uatom from test to the $ADDR
-    gaiad --home $NODE1_HOME tx bank send test $ADDR 1uatom --keyring-backend test --chain-id $CHAIN_ID --home $NODE1_HOME --sequence $SEQUENCE --node http://node1:26657 -y
-
-    # INDEX=$(( INDEX + 1))
-    SEQUENCE=$(( SEQUENCE + 1))
-
-    # sleep 1
+    gaiad --home $NODE1_HOME tx bank send test $ADDR 1uatom --keyring-backend test --chain-id $CHAIN_ID --home $NODE1_HOME -b block --node http://node1:26657 -y
+    semaphore release --name ${lockname}
+  ) &
 done
